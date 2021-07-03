9abd0c3e32bd8269934fdbfc3ed53792e872426e
inspiration33 <inspiration33@kakao.com>
2021-07-04 03:12:47 +0900

    Initial Commit

---
+++ CoffeeTime/CoffeeTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeTimeApp.swift
+//  CoffeeTime
+//
+//  Created by hansangil on 2021/07/04.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeTime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeeTime
+//
+//  Created by hansangil on 2021/07/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
